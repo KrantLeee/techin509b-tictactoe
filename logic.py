668f359cc7346b9ac4c20596e743dd04fb316a99
KrantLeee <silenceindeep3@gmail.com>
2023-11-02 06:20:15 +0800

    delete the LICENSE

--- logic.py
+++ logic.py
@@ -14,7 +14,35 @@ def make_empty_board():
 def get_winner(board):
     """Determines the winner of the given board.
     Returns 'X', 'O', or None."""
-    return None  # FIXME
+    
+    # Count X's and O's on the board
+    num_X = sum(row.count('X') for row in board)
+    num_O = sum(row.count('O') for row in board)
+
+    board[x][y] = 'X' if num_X == num_O else 'O'
+
+    #Check rows
+    for row in board:
+        if row[0] is not None and len(set(row)) == 1:
+            winner = row[0]
+            break
+
+    #Check columns
+    for col in range((len(board))):
+        column = [board[i][col] for i in range(len(board))]
+        if column[0] is not None and len(set(column)) == 1:
+            winner = column[0] 
+            break
+
+    #Check diagonals
+    if board[0][0] is not None and board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+        winner = board[0][0]
+    elif board[0][2] is not None and board[0][2] == board[1][1] and board[1][1] == board[2][0]:
+        winner = board[0][2]
+
+    # Check for a draw
+    if all(cell is not None for row in board for cell in row) and winner is None:
+        print("It's a draw!")
 
 
 def other_player(player):
