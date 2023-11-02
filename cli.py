d411f680b6d016b48100fc60eb9691ed361d2111
KrantLeee <silenceindeep3@gmail.com>
2023-11-02 12:01:07 +0800

    modified:   __pycache__/logic.cpython-311.pyc modified:   cli.py modified:   logic.py

--- cli.py
+++ cli.py
@@ -2,74 +2,52 @@
 # the Tic-Tac-Toe game. This is where input and output happens.
 # For core game logic, see logic.py.
 
-from logic import make_empty_board
+import logic
+
+# User Interaction
+def show_board(board):
+    for row in board:
+        print(row)
+
+def get_user_move(board):
+    while True:
+        user_input = input('Please make your movement by typing x,y (e.g., 1,2):').split(',')
+        if len(user_input) == 2 and user_input[0].isdigit() and user_input[1].isdigit(): # Check if the input is legal
+            x, y = int(user_input[0]), int(user_input[1])
+            if 0 <= x <= 2 and 0 <= y <= 2: # Check if the input is within boundary
+                if board[x][y] is None:  # Check if the position is empty
+                    return x, y
+                else:
+                    print("This position is already taken. Please choose another one.")
+            else:
+                print("Invalid coordinates. Please enter values between 0 and 2.")
+        else:
+            print("Invalid input. Please enter in the format x,y.")
 
 # Reminder to check all the tests
 
 if __name__ == '__main__':
-    board = make_empty_board()
+    board = logic.make_empty_board()
     winner = None
     print("TODO: take a turn!")
+    show_board(board)
     while winner == None:
-        
-       
-
-        # TODO: Show the board to the user.
-        for a in range(len(board)):    
-            print (board[a]) 
+        next_player = logic.next_player(board)
 
         # TODO: Input a move from the player.
-        while True: 
-            user_input = input('Please make your movement by typing x,y (e.g., 1,2):').split(',')
-            if len(user_input) == 2 and user_input[0].isdigit() and user_input[1].isdigit(): # Check if the input is legal
-                x, y = int(user_input[0]), int(user_input[1])
-                if 0 <= x <= 2 and 0 <= y <= 2: # Check if the input is within boundary
-                    if board[x][y] is None:  # Check if the position is empty
-                        break
-                    else:
-                        print("This position is already taken. Please choose another one.")
-                else:
-                    print("Invalid coordinates. Please enter values between 0 and 2.")
-            else:
-                print("Invalid input. Please enter in the format x,y.")
+        x, y = get_user_move(board)
+        board[x][y] = next_player
 
-        # Count X's and O's on the board
-        num_X = sum(row.count('X') for row in board)
-        num_O = sum(row.count('O') for row in board)
-
-        board[x][y] = 'X' if num_X == num_O else 'O'
-
-        #Check rows
-        for row in board:
-            if row[0] is not None and len(set(row)) == 1:
-                winner = row[0]
-                break
-
-        #Check columns
-        for col in range((len(board))):
-            column = [board[i][col] for i in range(len(board))]
-            if column[0] is not None and len(set(column)) == 1:
-                winner = column[0] 
-                break
-
-        #Check diagonals
-        if board[0][0] is not None and board[0][0] == board[1][1] and board[1][1] == board[2][2]:
-            winner = board[0][0]
-        elif board[0][2] is not None and board[0][2] == board[1][1] and board[1][1] == board[2][0]:
-            winner = board[0][2]
+        #TODO: Check winner.
+        winner = logic.get_winner(board, next_player)
 
         # Check for a draw
-        if all(cell is not None for row in board for cell in row) and winner is None:
-            print("It's a draw!")
-            break
+        logic.check_draw(board)
 
         # TODO: Update the board.
+        show_board(board)
 
         # TODO: Update who's turn it is.
-        if winner is None:
-          print("It's X's turn") if board[x][y] == 'O' else print("It's O's turn")
-        else:
-          break
-
+        print(f"It's {logic.next_player(board)}'s turn!")
 
     print ('The winner is', winner)
--- logic.py
+++ logic.py
@@ -2,7 +2,7 @@
 # or output happens here. The logic in this file
 # should be unit-testable.
 
-
+#Business Logic
 def make_empty_board():
     return [
         [None, None, None],
@@ -11,40 +11,26 @@ def make_empty_board():
     ]
 
 
-def get_winner(board):
+def get_winner(board, player):
     """Determines the winner of the given board.
     Returns 'X', 'O', or None."""
     
-    # Count X's and O's on the board
+    # Check rows, columns, and diagonals
+    winning = any(
+        (board[i][0] == board[i][1] == board[i][2] == player) or  # rows
+        (board[0][i] == board[1][i] == board[2][i] == player) or  # columns
+        (board[0][0] == board[1][1] == board[2][2] == player) or  # top-left to bottom-right diagonal
+        (board[0][2] == board[1][1] == board[2][0] == player)    # top-right to bottom-left diagonal
+        for i in range(3)
+    )
+    return player if winning else None
+
+def check_draw(board):
+    return all(cell is not None for row in board for cell in row)
+
+def next_player(board):
+    """Count the current X and O numbers to find the next player."""
     num_X = sum(row.count('X') for row in board)
     num_O = sum(row.count('O') for row in board)
+    return 'X' if num_X == num_O else 'O'
 
-    board[x][y] = 'X' if num_X == num_O else 'O'
-
-    #Check rows
-    for row in board:
-        if row[0] is not None and len(set(row)) == 1:
-            winner = row[0]
-            break
-
-    #Check columns
-    for col in range((len(board))):
-        column = [board[i][col] for i in range(len(board))]
-        if column[0] is not None and len(set(column)) == 1:
-            winner = column[0] 
-            break
-
-    #Check diagonals
-    if board[0][0] is not None and board[0][0] == board[1][1] and board[1][1] == board[2][2]:
-        winner = board[0][0]
-    elif board[0][2] is not None and board[0][2] == board[1][1] and board[1][1] == board[2][0]:
-        winner = board[0][2]
-
-    # Check for a draw
-    if all(cell is not None for row in board for cell in row) and winner is None:
-        print("It's a draw!")
-
-
-def other_player(player):
-    """Given the character for a player, returns the other player."""
-    return "O"  # FIXME
